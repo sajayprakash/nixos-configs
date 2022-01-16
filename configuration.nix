85c4d5b81b81f98a47edf50f52fb2fabd1a7a134
Sajay <78424701+sajayprakash@users.noreply.github.com>
2022-01-16 17:01:43 +0530

    update configs
    added ntfs support, added vscodium,python3,tree packages

--- configuration.nix
+++ configuration.nix
@@ -68,6 +68,9 @@
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
 
+  # Enable ntfs support
+  boot.supportedFilesystems = [ "ntfs" ];
+
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.s = {
     isNormalUser = true;
@@ -77,8 +80,13 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    vim
+    tree
     wget
+    #git
+    gitAndTools.gitFull
+    vscodium
+    python3
     firefox
     brave
     libreoffice 
