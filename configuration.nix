cdd6394369980be50a79a14273f15b2de7683a3e
Sajay <78424701+sajayprakash@users.noreply.github.com>
2022-01-16 12:07:25 +0530

    add nvidia drivers, discord and brave browser

--- configuration.nix
+++ configuration.nix
@@ -80,12 +80,14 @@
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
     firefox
+    brave
     libreoffice 
     arc-theme
     nano
     papirus-icon-theme
     neofetch
     htop
+    discord
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
